c234c54a9797b5a42c7fbe3e3e29d88c390c5811
Pavel Todorov <pavel.e.todorov@gmail.com>
2022-07-08 21:00:41 +0300

    Add foundation for HomeModel

--- Movies/Movies/Scenes/Home/HomeModel.swift
+++ Movies/Movies/Scenes/Home/HomeModel.swift
@@ -5,8 +5,52 @@
 //  Created by Pavel on 8.07.22.
 //
 
-import Foundation
+import UIKit
 
+// MARK: - Home Model
 struct HomeModel {
+    // MARK: Initializers
+    private init() {}
     
+    // MARK: Layout
+    struct Layout {
+        // MARK: Properties
+        
+        // MARK: Initializers
+        private init() {}
+    }
+
+    // MARK: Colors
+    struct Colors {
+        // MARK: Properties
+        static var background: UIColor? { .systemBackground }
+        
+        // MARK: Initializers
+        private init() {}
+    }
+
+    // MARK: Fonts
+    struct Fonts {
+        // MARK: Properties
+        
+        // MARK: Initializers
+        private init() {}
+    }
+
+    // MARK: Animations
+    struct Animations {
+        // MARK: Properties
+        
+        // MARK: Initializers
+        private init() {}
+    }
+
+    // MARK: Misc
+    struct Misc {
+        // MARK: Properties
+        
+        // MARK: Initializers
+        private init() {}
+    }
 }
+
