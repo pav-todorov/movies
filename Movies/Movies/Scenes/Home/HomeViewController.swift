519559220d5a7838720899dbfdc8e8686f7eaa3f
Pavel Todorov <pavel.e.todorov@gmail.com>
2022-07-08 21:13:16 +0300

    Add dependencies in HomeViewController

--- Movies/Movies/Scenes/Home/HomeViewController.swift
+++ Movies/Movies/Scenes/Home/HomeViewController.swift
@@ -8,10 +8,51 @@
 import UIKit
 
 // MARK: - Home View Controller
-class HomeViewController: UIViewController, HomeViewable {
+final class HomeViewController:
+    UIViewController,
+    HomeViewable, HomeNavigable
+{
+    // MARK: Subviews
 
+    // MARK: Properties
+    var presenter: (any HomePresentable)!
+    
+    private typealias UIModel = HomeModel
+
+    // MARK: Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBackground
+        setUp()
     }
+
+    // MARK: Setup
+    private func setUp() {
+        setUpView()
+        addSubviews()
+        setUpLayout()
+        setUpNavBar()
+    }
+    
+    private func setUpView() {
+        view.backgroundColor = UIModel.Colors.background
+    }
+
+    private func addSubviews() {
+
+    }
+
+    private func setUpLayout() {
+        NSLayoutConstraint.activate([
+        
+        ])
+    }
+    
+    private func setUpNavBar() {
+        navigationItem.title = "Home"
+    }
+
+    // MARK: Viewable
+
+    // MARK: Navigable
+
 }
