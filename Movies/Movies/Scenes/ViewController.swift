2270e44af9071c6a25b7c66eed1d7cffbb5e1a05
Pavel Todorov <pavel.e.todorov@gmail.com>
2022-07-08 20:44:47 +0300

    Initial commit

---
+++ Movies/Movies/Scenes/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Movies
+//
+//  Created by Pavel Todorov on 8.07.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+}
