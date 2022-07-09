87fd759026a61f955a8b5b310cd18f809610c818
Pavel Todorov <pavel.e.todorov@gmail.com>
2022-07-09 11:02:22 +0300

    Add Keys

---
+++ Movies/Movies/Helpers/Keys.swift
@@ -0,0 +1,22 @@
+//
+//  Keys.swift
+//  Movies
+//
+//  Created by Pavel on 9.07.22.
+//
+
+import Foundation
+
+// MARK: - Keys
+public struct Keys {
+    // MARK: Properties
+    static let key = APIKey.TMDBkey
+    static let baseURL: String = "https://api.themoviedb.org/"
+    static let baseAPIURL: String = "\(baseURL)/3/"
+    
+    // MARK: Notifications
+    struct Notifications {}
+    
+    // MARK: Initializers
+    private init() {}
+}
